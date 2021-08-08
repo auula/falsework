e06aad0c0c22ac1f8a2b7304e98688557089103f
Dings <deen.job@qq.com>
2021-08-08 18:18:10 +0800

    fix: explicit lifetime required in the type of `cmd`.

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use falsework::cmd::Command;
+    use falsework::cmd;
 
     /// Adds one to the number given.
     ///
@@ -20,23 +20,25 @@ mod tests {
     #[test]
     fn test_new() {
         let mut app = falsework::cli::new();
+
         println!("{:#?}", app);
+
         app.name("new name")
             .author("Leon Ding <ding@ibyte.me>")
             .version("0.0.2")
             .description("a command line program built with Falsework");
 
-        app.add_cmd(Command {
+        app.add_cmd(cmd::Command {
             run: || println!("run 1"),
             r#use: "cmd1",
         });
 
-        let cmd_list: &[Command] = &[
-            Command {
+        let cmd_list = &[
+            cmd::Command {
                 run: || println!("run 2"),
                 r#use: "cmd2",
             },
-            Command {
+            cmd::Command {
                 run: || println!("run 3"),
                 r#use: "cmd3",
             }
