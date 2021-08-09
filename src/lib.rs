44be78adb2667a7fb6b8e7addb289c2b2041ad68
Dings <deen.job@qq.com>
2021-08-09 22:08:16 +0800

    add: command unit testing.

--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,25 @@ pub mod cli {
     }
 
     impl<'a, 'c> App<'a, 'c> {
-        pub fn name(&mut self, name: &'a str) -> &mut App<'a, 'c> {
+        pub fn name(&mut self, name: &'a str) -> &mut Self {
             self.name = name;
             self
         }
-        pub fn author(&mut self, author: &'a str) -> &mut App<'a, 'c> {
+        pub fn author(&mut self, author: &'a str) -> &mut Self {
             self.author = author;
             self
         }
-        pub fn version(&mut self, version: &'a str) -> &mut App<'a, 'c> {
+        pub fn version(&mut self, version: &'a str) -> &mut Self {
             self.version = version;
             self
         }
 
-        pub fn description(&mut self, description: &'a str) -> &mut App<'a, 'c> {
+        pub fn description(&mut self, description: &'a str) -> &mut Self {
             self.description = description;
             self
         }
 
-        pub fn add_cmd(&mut self, cmd: Command<'c>) -> &mut App<'a, 'c> {
+        pub fn add_cmd(&mut self, cmd: Command<'c>) -> &mut Self {
             self.commands.push(cmd);
             self
         }
@@ -72,15 +72,31 @@ pub mod cli {
 }
 
 pub mod cmd {
+    use std::collections::HashMap;
+    use std::error::Error;
+    use std::env::Args;
+    use std::io;
+
+    pub type RunFunc = fn(
+        ctx: Context, args: Vec<String>,
+    ) -> Result<(), Box<dyn Error>>;
+
     #[derive(Debug)]
+    pub struct Context {
+        pub stdout: io::Stdout,
+        pub stderr: io::Stderr,
+    }
+
+
+    #[derive(Debug, Default)]
     pub struct Command<'c> {
-        pub run: fn(),
+        pub run: Option<RunFunc>,
         // Long is the long message shown in the 'help <this-command>' output.
         pub long: &'c str,
         // Short is the short description shown in the 'help' output.
         pub short: &'c str,
         pub r#use: &'c str,
-        // pub flags: Vec<Flag<T>>,
+        pub flags: Vec<Flag>,
         // pub aliases: Vec<&'c str>,
     }
 
@@ -88,6 +104,9 @@ pub mod cmd {
         // pub fn flags(&mut self) -> &mut Vec<Flag<T>> {
         //     &mut self.flags
         // }
+        pub fn flags(&mut self) -> &mut Vec<Flag> {
+            &mut self.flags
+        }
     }
 
     #[derive(Debug)]
@@ -99,43 +118,53 @@ pub mod cmd {
     }
 
     #[derive(Debug)]
-    pub struct Flag<T> {
-        pub name: String,
+    pub struct Flag {
+        pub flag: String,
+        pub default: String,
         pub r#type: Types,
         pub usages: String,
-        pub value: Box<T>,
+        // pub value: Box<T>,
     }
 
-    impl<T> Flag<T> {
+    impl Flag {
         // s2s add -x 10 -y 10 = 20
         // -x i64 default= 10 usages 加数
-        pub fn string(&mut self, name: &str, default: T, usages: &str) {
-            self.name = name.parse().unwrap();
-            self.value = Box::new(default);
+
+        pub fn bound_string(&mut self, value: &mut String, flag: &str, default: &str, usages: &str) -> Result<(), Box<dyn Error>> {
             self.r#type = Types::STRING;
-            self.usages = usages.parse().unwrap()
+            self.usages = usages.parse().unwrap();
+            self.flag = flag.parse().unwrap();
+            // value = xx;
+            Ok(())
         }
 
-        pub fn int(&mut self, name: &str, default: T, usages: &str) {
-            self.name = name.parse().unwrap();
-            self.value = Box::new(default);
-            self.r#type = Types::I64;
-            self.usages = usages.parse().unwrap()
-        }
-
-        pub fn bool(&mut self, name: &str, default: T, usages: &str) {
-            self.name = name.parse().unwrap();
-            self.value = Box::new(default);
-            self.r#type = Types::BOOL;
-            self.usages = usages.parse().unwrap()
-        }
-
-        pub fn float(&mut self, name: &str, default: T, usages: &str) {
-            self.name = name.parse().unwrap();
-            self.value = Box::new(default);
-            self.r#type = Types::F64;
-            self.usages = usages.parse().unwrap()
-        }
+        // pub fn string(&mut self, name: &str, default: T, usages: &str) {
+        //     self.name = name.parse().unwrap();
+        //     self.value = Box::new(default);
+        //     self.r#type = Types::STRING;
+        //     self.usages = usages.parse().unwrap()
+        // }
+        //
+        // pub fn int(&mut self, name: &str, default: T, usages: &str) {
+        //     self.name = name.parse().unwrap();
+        //     self.value = Box::new(default);
+        //     self.r#type = Types::I64;
+        //     self.usages = usages.parse().unwrap()
+        // }
+        //
+        // pub fn bool(&mut self, name: &str, default: T, usages: &str) {
+        //     self.name = name.parse().unwrap();
+        //     self.value = Box::new(default);
+        //     self.r#type = Types::BOOL;
+        //     self.usages = usages.parse().unwrap()
+        // }
+        //
+        // pub fn float(&mut self, name: &str, default: T, usages: &str) {
+        //     self.name = name.parse().unwrap();
+        //     self.value = Box::new(default);
+        //     self.r#type = Types::F64;
+        //     self.usages = usages.parse().unwrap()
+        // }
     }
 }
 
--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,13 @@
 #[cfg(test)]
 mod tests {
     use falsework::cmd;
-    use falsework::cmd::Types;
+    use falsework::cmd::{
+        Types, Context,
+    };
+    use std::error::Error;
+    use std::io::Write;
+    use std::io;
+    use std::env;
 
     #[test]
     fn test_build_new() {
@@ -25,32 +31,56 @@ mod tests {
             .description("A command line program built with Falsework.");
 
         app.add_cmd(cmd::Command {
-            run: || println!("run 1"),
+            run: Option::Some(
+                |c, args| -> Result<(), Box<dyn Error>> {
+                    let mut handle = c.stdout.lock();
+                    handle.write_all(b"running");
+                    Ok(())
+                }
+            ),
             long: "Short is the short description shown in the 'help' output.",
             short: "cmd short",
             r#use: "cmd1",
+            ..Default::default()
         });
-        println!("{:#?}", app);
+
+        if let Some(cmd) = app.get_command("cmd1") {
+            match cmd.run {
+                None => {
+                    panic!("no run func")
+                }
+                Some(func) => {
+                    func(
+                        cmd::Context {
+                            stdout: io::stdout(),
+                            stderr: io::stderr(),
+                        },
+                        env::args().collect(),
+                    );
+                }
+            }
+        }
     }
 
     #[test]
     fn test_add_command_list() {
-
         let mut app = falsework::cli::new();
 
         app.commands(
             vec![
                 cmd::Command {
-                    run: || println!("running function by cmd 1"),
+                    run: Option::None,
                     long: "cmd 1 long help",
                     short: "cmd 1 short help",
                     r#use: "cmd 1",
+                    ..Default::default()
                 },
                 cmd::Command {
-                    run: || println!("running function by cmd 2"),
+                    run: Option::None,
                     long: "cmd 2 long help",
                     short: "cmd 2 short help",
                     r#use: "cmd 2",
+                    ..Default::default()
                 }
             ]
         );
@@ -61,11 +91,13 @@ mod tests {
     fn test_get_command_and_mut() {
         let mut app = falsework::cli::new();
 
+
         app.add_cmd(cmd::Command {
-            run: || println!("run 1"),
+            run: None,
             long: "Short is the short description shown in the 'help' output.",
             short: "cmd short",
             r#use: "cmd",
+            ..Default::default()
         });
 
         if let Some(cmd) = app.get_command_mut("cmd") {
@@ -76,5 +108,21 @@ mod tests {
         if let Some(cmd) = app.get_command("cmd") {
             assert_eq!(cmd.r#use, "cmd")
         }
+    }
+
+    #[test]
+    fn test_command_flags() {
+        let mut host: String;
+        let mut root_command = cmd::Command {
+            run: Option::None,
+            long: "Short is the short description shown in the 'help' output.",
+            short: "cmd short",
+            r#use: "cmd",
+            ..Default::default()
+        };
+
+        // root_command.flags()[1].bound_string(&mut host, "-h", "127.0.0.1", "主机参数");
+
+        // assert_eq!(host, "127.0.0.1");
     }
 }
